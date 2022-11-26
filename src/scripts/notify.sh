12a4ea0d8e5f3665073db387ef77cbd3e428e2bd
Akash <akash.thakare@thoughtworks.com>
2022-11-26 15:34:00 +0530

    Pass chat type and weebhook as parameters.

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -3,18 +3,20 @@
 # shellcheck source=/dev/null
 source /tmp/NORB_ENVS
 
-if [ "$NORB_STATUS" = "COMPLETED" ]; then
+if [ "$NORB_STATUS" = "SUCCESS" ]; then
   export NORB_IMAGE="https://i.ibb.co/Sw6v9YH/checked.png"
 else
   export NORB_IMAGE="https://i.ibb.co/Sw6v9YH/cancel.png"
 fi
 
-if [ -z "$GCHAT_WEBHOOK" ]; then
-  NORB_REQUEST=$(< gchat-request.json envsubst)
-  export NORB_REQUEST
+if [ "$TYPE" = "GCHAT" ]; then
+  if [ -z "$WEBHOOK" ]; then
+    NORB_REQUEST=$(< gchat-request.json envsubst)
+    export NORB_REQUEST
 
-  curl --header "Content-Type: application/json" \
-       --request POST \
-       --data "$NORB_REQUEST" \
-       "$GCHAT_WEBHOOK"
+    curl --header "Content-Type: application/json" \
+         --request POST \
+         --data "$NORB_REQUEST" \
+         "$GCHAT_WEBHOOK"
+  fi
 fi
