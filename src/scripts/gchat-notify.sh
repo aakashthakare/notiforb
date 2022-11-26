12aca700c12091c0257fbd33611aa7fd938372a4
Akash <akash.thakare@thoughtworks.com>
2022-11-26 16:06:12 +0530

    Modify notify sh.

--- src/scripts/gchat-notify.sh
+++ src/scripts/gchat-notify.sh
@@ -9,12 +9,12 @@ else
   export NORB_IMAGE="https://i.ibb.co/Sw6v9YH/cancel.png"
 fi
 
-if [ -z "$WEBHOOK" ]; then
+if [ -z "${WEBHOOK}" ]; then
   NORB_REQUEST=$(< gchat-request.json envsubst)
   export NORB_REQUEST
 
   curl --header "Content-Type: application/json" \
        --request POST \
        --data "$NORB_REQUEST" \
-       "$GCHAT_WEBHOOK"
+       "${WEBHOOK}"
 fi
