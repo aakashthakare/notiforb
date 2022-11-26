a4e91602104ecd7388ffe235f2ebe747c66dc54c
Akash <akash.thakare@thoughtworks.com>
2022-11-26 15:13:08 +0530

    Basic setup to send notification to google chat.

---
+++ src/scripts/notify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source /tmp/NORB_ENVS
+
+if [ "$NORB_STATUS" = "COMPLETED" ]; then
+  export NORB_IMAGE="https://i.ibb.co/Sw6v9YH/checked.png"
+else
+  export NORB_IMAGE="https://i.ibb.co/Sw6v9YH/cancel.png"
+fi
+
+if [ -z "$GCHAT_WEBHOOK" ]; then
+  NORB_REQUEST=$(cat gchat-request.json | envsubst)
+  export NORB_REQUEST
+
+  curl --header "Content-Type: application/json" \
+       --request POST \
+       --data "$NORB_REQUEST" \
+       "$GCHAT_WEBHOOK"
+fi
