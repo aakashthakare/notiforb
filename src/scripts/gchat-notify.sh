63cae2265d9643150061eb096dbb15e46e56a1e8
Akash <akash.thakare@thoughtworks.com>
2022-11-27 13:44:25 +0530

    Modify notify sh.

--- src/scripts/gchat-notify.sh
+++ src/scripts/gchat-notify.sh
@@ -13,7 +13,7 @@ if [ -z "${WEBHOOK}" ];
 then
   echo "Webhook URL is not provided."
 else
-  NORB_REQUEST=$(eval "echo \"$(cat scripts/gchat-request.json)\"")
+  NORB_REQUEST=$(eval "echo \"$(cat ./gchat-request.json)\"")
   export NORB_REQUEST
 
   curl --header "Content-Type: application/json" \
